9cd4f1dedc8e62ce6f8dd08a2158ed2fc9579396
Guanxiong Qu <quguanxiong@gmail.com>
2017-06-23 01:15:02 +0900

    Update E_Ms_int_single.m

--- E_Ms_int_single.m
+++ E_Ms_int_single.m
@@ -1,17 +1,31 @@
-function [E_2,Ms] = E_Ms_int_single(Gamma,L,T,J,h)
-    %%%E
-    E=0
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% single run Energy and Magnetization
+% 2017/6/23
+% Guanxiong Qu
+% quguanxiong@gmail.opcm
+%
+% Parameters:
+% Gamma: configuration
+% L:         Dimension
+% T:         Temperature
+% J:         Exchange
+% h:         bias field
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function [E,Ms] = E_Ms_int_single(Gamma,L,T,J,h)
+%%%Energy 
+    ij=0
     for i=1:L
         for j=1:L-1
-        E=E+Gamma(i,j)*Gamma(i,j+1)
+        ij=ij+Gamma(i,j)*Gamma(i,j+1)
         end
     end
         for j=1:L
         for i=1:L-1
-        E=E+Gamma(i,j)*Gamma(i+1,j)
+        ij=ij+Gamma(i,j)*Gamma(i+1,j)
         end
     end
-    E_2=-h*sum(sum(Gamma))-J*E
-
+    E=-h*sum(sum(Gamma))-J*ij
+%%% Magnetization
     Ms=sum(sum(Gamma))
 end
